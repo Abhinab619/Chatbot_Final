3356f190aea8920990b587c88c6ab8c18b4daedc
Abhinab Kumar <bca15001.22@bitmesra.ac.in>
2025-05-06 22:35:19 +0530

    data changed

--- backend/server_helpdesk_rec1.py
+++ backend/server_helpdesk_rec1.py
@@ -45,88 +45,21 @@ EMBEDDINGS_DIR = os.path.join(BASE_DIR, "Embeddings")
 
 
 
-# tool2
-vectorstore2 = Chroma(persist_directory=os.path.join(EMBEDDINGS_DIR, "tool2"),
+# tool10
+vectorstore10 = Chroma(persist_directory=os.path.join(EMBEDDINGS_DIR, "tool10"),
                      embedding_function=GoogleGenerativeAIEmbeddings(
                      model="models/text-embedding-004",
                      google_api_key="AIzaSyBgdymDNQMdnSEad-xYapzh1hS3F6wmxfE"))
-retriever2 = vectorstore2.as_retriever(search_type="mmr", search_kwargs={'k': 3, 'lambda_mult': 0.7})
-retriever_tool2 = create_retriever_tool(retriever=retriever2,                           
-                                       name="MMUY_scheme1_SC_ST",
-                                       description="Retrieves relevant information from stored documents summarizing all the information without missing any")
-
-# Tool 1.3
-vectorstore3 = Chroma(persist_directory=os.path.join(EMBEDDINGS_DIR, "tool3"),
-                     embedding_function=GoogleGenerativeAIEmbeddings(
-                     model="models/text-embedding-004",
-                     google_api_key="AIzaSyBgdymDNQMdnSEad-xYapzh1hS3F6wmxfE"))
-
-retriever3 = vectorstore3.as_retriever(search_type="mmr", search_kwargs={'k': 3, 'lambda_mult': 0.7})
-retriever_tool3 = create_retriever_tool(retriever=retriever3,                           
-                                       name="MMUY_scheme2_Extremely_Backward_Class",
-                                       description="Retrieves relevant information from stored documents summarizing all the information without missing any")
-
-# Tool 1.4
-vectorstore4 = Chroma(persist_directory=os.path.join(EMBEDDINGS_DIR, "tool4"),
-                     embedding_function=GoogleGenerativeAIEmbeddings(
-                     model="models/text-embedding-004",
-                     google_api_key="AIzaSyBgdymDNQMdnSEad-xYapzh1hS3F6wmxfE"))
-
-retriever4 = vectorstore4.as_retriever(search_type="mmr", search_kwargs={'k': 3, 'lambda_mult': 0.7})
-retriever_tool4 = create_retriever_tool(retriever=retriever4,                           
-                                       name="MMUY_scheme3_YUVA",
-                                       description="Retrieves relevant information from stored documents summarizing all the information without missing any")
-
-# Tool 1.5
-vectorstore5 = Chroma(persist_directory=os.path.join(EMBEDDINGS_DIR, "tool5"),
-                     embedding_function=GoogleGenerativeAIEmbeddings(
-                     model="models/text-embedding-004",
-                     google_api_key="AIzaSyBgdymDNQMdnSEad-xYapzh1hS3F6wmxfE"))
-
-retriever5 = vectorstore5.as_retriever(search_type="mmr", search_kwargs={'k': 3, 'lambda_mult': 0.7})
-retriever_tool5 = create_retriever_tool(retriever=retriever5,                           
-                                       name="MMUY_scheme4_Mahila",
-                                       description="Retrieves relevant information from stored documents summarizing all the information without missing any")
-
-# Tool 1.6
-vectorstore6 = Chroma(persist_directory=os.path.join(EMBEDDINGS_DIR, "tool6"),
-                     embedding_function=GoogleGenerativeAIEmbeddings(
-                     model="models/text-embedding-004",
-                     google_api_key="AIzaSyBgdymDNQMdnSEad-xYapzh1hS3F6wmxfE"))
-
-retriever6 = vectorstore6.as_retriever(search_type="mmr", search_kwargs={'k': 3, 'lambda_mult': 0.7})
-retriever_tool6 = create_retriever_tool(retriever=retriever6,                           
-                                       name="MMUY_scheme5_Alpsankhyak",
-                                       description="Retrieves relevant information from stored documents summarizing all the information without missing any")
-
-# Tool 1.7 (MMUY)
-vectorstore7 = Chroma(persist_directory=os.path.join(EMBEDDINGS_DIR, "tool7"),
-                     embedding_function=GoogleGenerativeAIEmbeddings(
-                     model="models/text-embedding-004",
-                     google_api_key="AIzaSyBgdymDNQMdnSEad-xYapzh1hS3F6wmxfE"))
-
-retriever7 = vectorstore7.as_retriever(search_type="mmr", search_kwargs={'k': 3, 'lambda_mult': 0.7})
-retriever_tool7 = create_retriever_tool(retriever=retriever7,                           
-                                       name="MMUY_Mukhyamantri_Udyami_Yojana",
-                                       description="Retrieves relevant information from stored documents summarizing all the information without missing any, Also state the Sub-Schemes under this")
-
-
-# Tool 1.8 (BLUY)
-vectorstore8 = Chroma(persist_directory=os.path.join(EMBEDDINGS_DIR, "tool8"),
-                     embedding_function=GoogleGenerativeAIEmbeddings(
-                     model="models/text-embedding-004",
-                     google_api_key="AIzaSyBgdymDNQMdnSEad-xYapzh1hS3F6wmxfE"))
-
-retriever8 = vectorstore8.as_retriever(search_type="mmr", search_kwargs={'k': 3, 'lambda_mult': 0.7})
-retriever_tool8 = create_retriever_tool(retriever=retriever8,                           
-                                       name="BLUY_Bihar_Laghu_Udyami_Yojna",
-                                       description="Retrieves relevant information from stored documents summarizing all the information without missing any")
+retriever10 = vectorstore10.as_retriever(search_type="mmr", search_kwargs={'k': 3, 'lambda_mult': 0.7})
+retriever_tool10 = create_retriever_tool(retriever=retriever10,                           
+                                       name="Udyami_Yojna",
+                                       description="You are an expert assistant for the Udyami Yojna scheme. Using the information retrieved from your knowledge base, provide complete and accurate answers related to the Mukhyamantri Udyami Yojna, including but not limited to: scheme overview, project types supported, eligibility criteria, required documents, step-by-step application and selection process, financial assistance and benefits,fund disbursement, training and installment procedures, loan repayment guidelines, and any important conditions or restrictions. Summarize all relevant details concisely without omitting key points.")
 
 
 
 
 
-# Direct Gemini Tool (tool 8)
+# Direct Gemini Tool (tool 9)
 chat = ChatGoogleGenerativeAI(model="gemini-1.5-pro",
                               google_api_key="AIzaSyBgdymDNQMdnSEad-xYapzh1hS3F6wmxfE")
 
@@ -142,7 +75,7 @@ def direct_llm_answer(query: str) -> str:
     response = chat.invoke(prompt)
     return response
 
-tools = [retriever_tool2, retriever_tool3, retriever_tool4, retriever_tool5, retriever_tool6, retriever_tool7, retriever_tool8, direct_llm_answer]
+tools = [retriever_tool10, direct_llm_answer]
 
 chat_prompt_template = hub.pull("hwchase17/openai-tools-agent")
 agent = create_tool_calling_agent(llm=chat, tools=tools, prompt=chat_prompt_template)
